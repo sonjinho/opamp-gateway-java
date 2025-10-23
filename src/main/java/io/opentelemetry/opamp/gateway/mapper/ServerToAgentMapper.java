3143702b24a5738046f88c46bd15d0997124eb9c
sonjinho <sonjinhoho@naver.com>
2025-10-23 16:33:38 +0900

    init mapper

--- src/main/java/io/opentelemetry/opamp/gateway/mapper/ServerToAgentMapper.java
+++ src/main/java/io/opentelemetry/opamp/gateway/mapper/ServerToAgentMapper.java
@@ -1,4 +1,11 @@
 package io.opentelemetry.opamp.gateway.mapper;
 
+import io.opentelemetry.opamp.gateway.domain.server.ServerToAgentDomain;
+import opamp.proto.Opamp;
+
 public class ServerToAgentMapper {
+
+    public Opamp.ServerToAgent mapperToProto(ServerToAgentDomain serverToAgentDomain) {
+        return Opamp.ServerToAgent.newBuilder().build();
+    }
 }
