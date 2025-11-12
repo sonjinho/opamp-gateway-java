2640ae9cb3c1aefab84d0e6e161aa53dde9ba7b0
sonjinho <sonjinhoho@naver.com>
2025-11-12 11:36:49 +0900

    backup

--- src/main/java/io/opentelemetry/opamp/receiver/domain/OwnTelemetrySetting.java
+++ src/main/java/io/opentelemetry/opamp/receiver/domain/OwnTelemetrySetting.java
@@ -17,24 +17,4 @@ public record OwnTelemetrySetting(
         OwnTelemetryConnectionSetting ownMetricSetting,
         OwnTelemetryConnectionSetting ownLogSetting
 ) {
-    public OwnTelemetrySetting.Summary toSummary() {
-        return new Summary(
-                id,
-                type,
-                labels,
-                ownMetricEnable,
-                ownTraceEnable,
-                ownLogEnable
-        );
-    }
-
-    record Summary(
-            UUID id,
-            String type,
-            Map<String, String> labels,
-            Boolean ownMetricEnable,
-            Boolean ownTraceEnable,
-            Boolean ownLogEnable
-    ) {
-    }
 }
